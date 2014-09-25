e592d2293811e95c4ec57afc31a4b4dd6798e6b3
Kaito Tokumori <k148574@ie.u-ryukyu.ac.jp>
2014-09-25 12:35:46 +0000

    add sold out search function.

--- app/controllers/sold_out_controller.rb
+++ app/controllers/sold_out_controller.rb
@@ -1,2 +1,37 @@
+require 'open-uri'
+require 'json'
+
 class SoldOutController < ApplicationController
+  def index
+    application_id = ENV["SOLD_OUT_APPID"]
+    affiliate_id = ENV["SOLD_OUT_AFID"]
+
+    item_search_url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222"
+    genreId = 101240
+    max_page = 100 # Rakuten API's maximum number
+    sort_style = "-updateTimestamp"
+    page_num = 1
+    hit_num = 30
+    search_flag = true
+    while page_num <= max_page and search_flag
+    
+      begin
+        response = open "#{item_search_url}?applicationId=#{application_id}&genreId=#{genreId}&page=#{page_num}&hits=#{hit_num}&availability=0&sort=#{sort_style}"
+      rescue OpenURI::HTTPError => error
+        error_status, error_message = error.io.status
+        p "error : #{error_status} #{error_message}"
+        break
+      end
+
+      result = ActiveSupport::JSON.decode response.read
+      max_page = result['pageCount'].to_i
+      result['Items'].each do |item|
+        if item['Item']['availability'] == 0
+          search_flag = false
+          p item['Item']['itemName']
+        end
+      end
+      page_num = page_num + 1
+    end
+  end
 end
