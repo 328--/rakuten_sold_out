3837d75eefe043bc88944e4ac9ef16f54a8d4176
kkz_interface <k148584@ie.u-ryukyu.ac.jp>
2014-09-26 04:10:22 +0000

    Introduce bootstrap

--- app/controllers/sold_out_controller.rb
+++ app/controllers/sold_out_controller.rb
@@ -1,2 +1,11 @@
 class SoldOutController < ApplicationController
+
+  def index
+    @genre_list = [["hoge", 1], ["fuga", 2], ["piyo", 3]]
+  end
+  
+  def show
+    puts "HOGEEEEEEEEEEEEEEEEEEEEEEEEE"
+  end
+
 end
